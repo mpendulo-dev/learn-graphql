99ab7c76489acd830cbcc5aad670be04c052095a
mpendulo-dev <mpendulo.dev@gmail.com>
2023-01-18 10:00:13 +0200

    Created mutation to add books and authors

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -77,6 +77,45 @@ const RootQuery = new GraphQLObjectType({
     }
 })
 
+//mutation - change data (CREATE, UPDATE, DELETE)
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: { type: GraphQLString },
+                age: { type : GraphQLInt }
+            },
+            resolve(parent, args) {
+                //Author is the mongodb model
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                })
+                return author.save()
+            }
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: { type: GraphQLString },
+                genre: { type: GraphQLString },
+                authorId: { type: GraphQLID }
+            },
+            resolve(parent, args) {
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId
+                })
+                return book.save()
+            }
+        }
+    }
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 })
