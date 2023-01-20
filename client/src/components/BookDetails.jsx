94be66bd6b3b4a827088b9e7aff096b6be47bfa7
mpendulo-dev <mpendulo.dev@gmail.com>
2023-01-20 10:56:43 +0200

    Fix styling for the book list

--- client/src/components/BookDetails.jsx
+++ client/src/components/BookDetails.jsx
@@ -1,48 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import { useQuery, useMutation } from '@apollo/client';
-import { GET_BOOK } from '../GraphQL/Queries';
+import React, { useEffect, useState } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_BOOK } from "../GraphQL/Queries";
 
 const BookDetails = ({ bookId }) => {
-	const { error, loading, data } = useQuery(GET_BOOK, {
-		variables: {
-			id: bookId,
-		},
-		refetchQueries: [GET_BOOK],
-	});
-	const [authorBook, setAuthorBook] = useState([]);
+    const { error, loading, data } = useQuery(GET_BOOK, {
+        variables: {
+            id: bookId,
+        },
+        refetchQueries: [GET_BOOK],
+    });
+    const [authorBook, setAuthorBook] = useState([]);
 
-	useEffect(() => {
-		setAuthorBook(data !== undefined ? data.book : []);
-	}, [data]);
+    useEffect(() => {
+        setAuthorBook(data !== undefined ? data.book : []);
+    }, [data]);
 
-	return (
-		<div id='book-details'>
-			{loading && <p>Loading...</p>}
+    return (
+        <div id="book-details">
+            {loading && <p>Loading...</p>}
 
-			{authorBook.length !== 0 ? (
-				<div>
-					<p>Output book details</p>
-					<h2>Book name: {authorBook.name}</h2>
-					<p>Genre: {authorBook.genre}</p>
-					<p>Author name: {authorBook.author.name}</p>
-					<p>Author age: {authorBook.author.age}</p>
-					<p>Books written by Author: </p>
-					{authorBook &&
-						authorBook.author.books.map(books => (
-							<>
-								<ul className='other-books'>
-									<li key={books.id}>{books.name}</li>
-								</ul>
-							</>
-						))}
-				</div>
-			) : (
-				<>
-					<p>No book selected ...</p>
-				</>
-			)}
-		</div>
-	);
+            {authorBook.length !== 0 ? (
+                <>
+                    <p>Output book details</p>
+                    <h2>Book name: {authorBook.name}</h2>
+                    <p>Genre: {authorBook.genre}</p>
+                    <p>Author name: {authorBook.author.name}</p>
+                    <p>Author age: {authorBook.author.age}</p>
+                    <p>Books written by Author: </p>
+                    {authorBook &&
+                        authorBook.author.books.map((books) => (
+                            <ul className="other-books" key={books.id}>
+                                <li>{books.name}</li>
+                            </ul>
+                        ))}
+                </>
+            ) : (
+                <>
+                    <p>No book selected ...</p>
+                </>
+            )}
+        </div>
+    );
 };
 
 export default BookDetails;
--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,38 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import { useQuery, gql } from '@apollo/client';
-import { GET_BOOKS } from '../GraphQL/Queries';
-import BookDetails from '../components/BookDetails';
+import React, { useEffect, useState } from "react";
+import { useQuery, gql } from "@apollo/client";
+import { GET_BOOKS } from "../GraphQL/Queries";
+import BookDetails from "../components/BookDetails";
 
 const BookList = () => {
-	const { error, loading, data } = useQuery(GET_BOOKS);
-	const [books, setBooks] = useState([]);
-	const [bookId, setBookId] = useState('');
+    const { error, loading, data } = useQuery(GET_BOOKS);
+    const [books, setBooks] = useState([]);
+    const [bookId, setBookId] = useState("");
 
-	useEffect(() => {
-		setBooks(data && data.books);
-	}, [data]);
+    useEffect(() => {
+        setBooks(data && data.books);
+    }, [data]);
 
-	const getBookId = id => {
-		setBookId(id && id);
-	};
+    const getBookId = (id) => {
+        setBookId(id && id);
+    };
 
-	return (
-		<>
-			<div>
-				{loading && <p>Loading...</p>}
-				{error && <p>Error: {error.message}</p>}
-				{books &&
-					books.map(data => (
-						<ul key={data.id} id='book-list' className='container'>
-							<li onClick={() => getBookId(data.id)}>
-								{data.name}
-							</li>
-						</ul>
-					))}
-			</div>
-			<BookDetails bookId={bookId} />
-		</>
-	);
+    return (
+        <>
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error.message}</p>}
+            {books &&
+                books.map((data) => (
+                    <ul key={data.id} id="book-list">
+                        <li onClick={() => getBookId(data.id)}>{data.name}</li>
+                    </ul>
+                ))}
+            <BookDetails bookId={bookId} />
+        </>
+    );
 };
 
 export default BookList;
