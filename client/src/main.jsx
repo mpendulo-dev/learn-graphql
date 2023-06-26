bc49c0012ac037c04b1dd9100fdaed5c0f9b41c5
Mpendulo Sibiya <mpendulodev@gmail.com>
2023-06-26 21:39:59 +0200

    404 Error because endpoint incorrect

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ import {
 } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "https://graphql-server-0cmr.onrender.com",
+    uri: "https://graphql-server-0cmr.onrender.com/graphql",
     cache: new InMemoryCache(),
 });
 
